0c68a40986df15d97ad722e0627a885968fea539
Trinh Le (tl0288) <tl0288@att.com>
2018-06-28 14:45:16 -0500

    simple template

---
+++ Object.md
@@ -0,0 +1,2 @@
+1. Instead of using `var`, use `let` if you want to redefine the variable or `const` if you do not need to redefine the variable.
+2. 
---
+++ index.js
@@ -0,0 +1,13 @@
+// this is an object literal
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function () {
+        console.log('draw');
+    }
+}
+
+circle.draw();
